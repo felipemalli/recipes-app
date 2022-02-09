7bfde51f2faf80deceb7c625aa81cadefc946531
raphael-caninde <raphael.caninde@gmail.com>
2022-02-08 22:45:55 -0300

    agora vai

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import requestAPI from '../services/requestAPI';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function ExploreDrinks({ history }) {
+  const [drinkId, setDrinkId] = useState('');
+
+  useEffect(() => {
+    requestAPI.getDrinks.infoRandom().then((v) => setDrinkId(v.drinks[0].idDrink));
+  }, []);
+
   return (
     <div>
       <Header title="Explore Drinks" haveSearch={ false } get="a" />
@@ -18,6 +25,7 @@ function ExploreDrinks({ history }) {
         <button
           type="button"
           data-testid="explore-surprise"
+          onClick={ () => history.push(`/drinks/${drinkId}`) }
         >
           Surprise me!
         </button>
